681071e8f3e37f5c838a002a7ef65a6153667ccf
Chahat Bhatia <chahatbhatia90@gmail.com>
2021-03-09 23:01:10 +0530

    Whole project upload

---
+++ config/db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(db, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true
+		});
+
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
+
+
+//  cl ID  
+// f8545aa00d92b69872b2
+
+// cl Pass
+// ca74bf04e3501f5e9f9f55685f95ec5ee321e21c
